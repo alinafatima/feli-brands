44b8e521d2c0675ae3453ea523f993cd39f28ab8
alinafatima <alina.fatima.work@gmail.com>
2023-06-17 15:17:30 +0500

    Deploy to Github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Specify your custom "exportPathMap" function
+  generateStaticRoutes: async function () {
+    return {
+      '/': { page: '/' },
+      // Add other routes here if needed
+    };
+  },
+  // Specify the location of the export directory
+  distDir: 'out',
+  // Add any other custom configuration options if necessary
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve('./'));
+    return config;
+  },
+};
 
 module.exports = nextConfig
