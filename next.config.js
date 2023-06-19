fa2fc69dbfd33509a23234f071c27ee85269ca45
alinafatima <alina.fatima.work@gmail.com>
2023-06-19 10:32:47 +0500

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Specify your custom "exportPathMap" function
-  generateStaticRoutes: async function () {
-    return {
-      '/': { page: '/' },
-      // Add other routes here if needed
-    };
-  },
-  // Specify the location of the export directory
-  distDir: 'out',
-  // Add any other custom configuration options if necessary
-  webpack: (config) => {
-    config.resolve.modules.push(path.resolve('./'));
-    return config;
-  },
-};
+const nextConfig = {}
 
 module.exports = nextConfig
